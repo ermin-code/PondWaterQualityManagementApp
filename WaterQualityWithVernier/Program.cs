6a8a7328a8d6e145c4398435a24c03eb42c0d59b
Ermin Vila <83635556+ermin-code@users.noreply.github.com>
2021-10-22 00:48:28 -0400

    Finally fixed the issue I was having with user data not being recorded into a text file. I used list string manipulation to record date and temperature user data into CSV file.

--- WaterQualityWithVernier/Program.cs
+++ WaterQualityWithVernier/Program.cs
@@ -25,16 +25,15 @@ namespace WaterQualityWithVernier
             Console.WriteLine("");
             Console.WriteLine("*********************** Welcome to Pond Water Quality with Vernier V.1.0 ***********************");
             Console.WriteLine("");
-            Console.WriteLine("Option 1. Temperature Experiment (Stainless Steel Temperature Probe Required)");
-            Console.WriteLine("Option 2. pH Experiment (pH Sensor Required)");
-            Console.WriteLine("Option 3. Turbidity Experiment (Turbidity Sensor Required)");
-            Console.WriteLine("Option 4. Dissolved Oxygen Experiment (Dissolved Oxygen Sensor Required)");
-            Console.WriteLine("Option 5. Experimental Analysis");
+            Console.WriteLine("     Option 1. Temperature Experiment (Stainless Steel Temperature Probe Required)");
+            Console.WriteLine("     Option 2. pH Experiment (pH Sensor Required)");
+            Console.WriteLine("     Option 3. Turbidity Experiment (Turbidity Sensor Required)");
+            Console.WriteLine("     Option 4. Dissolved Oxygen Experiment (Dissolved Oxygen Sensor Required)");
+            Console.WriteLine("     Option 5. Experimental Analysis");
             Console.WriteLine("");
-            Console.WriteLine("Option 6. Exit The Program");
+            Console.WriteLine("     Option 6. Exit The Program");
             Console.WriteLine("");
-            Console.WriteLine("Please type which option you would like to choose:");
-
+            Console.WriteLine("     Please type which option you would like to choose:");
 
             string myoptions;
             myoptions = Console.ReadLine();
@@ -66,7 +65,6 @@ namespace WaterQualityWithVernier
             }
 
             MainMenu();
-
         }
 
         //Temperature Experiment Section
@@ -75,34 +73,34 @@ namespace WaterQualityWithVernier
             Console.Clear();
             Console.WriteLine("*********************** Temperature Experiment ***********************");
             Console.WriteLine("");
-            Console.WriteLine("In this experiment you will collect temperature data on your pond water using Vernier temperature probe, Texas Instruments Graphing Calculator with attached CBL2 Calculator-Based Laboratory.");
-            Console.WriteLine("Outdoor water temperatures can range from 32 degrees Celsius to above 86 degrees Celsius in the summer. In general, cooler water in pond is generally considered healthier than warmer water.");
-            Console.WriteLine("Pond water temperature plays a vital role in the overal health of the eco-system and the health of pond fish. The perfect pond water should be in the range of 68 to 74 degrees Fahrenheit.");
-            Console.WriteLine("During summer, water loses much of its ability to hold oxygen when the temperature is above 85 degrees. In winter, when the temperature goes below 39 degrees Fahrenheit fish do not have to eat.");
+            Console.WriteLine(" In this experiment you will collect temperature data on your pond water using Vernier temperature probe, Texas Instruments Graphing Calculator with attached CBL2 Calculator-Based Laboratory.");
+            Console.WriteLine(" Outdoor water temperatures can range from 32 degrees Celsius to above 86 degrees Celsius in the summer. In general, cooler water in pond is generally considered healthier than warmer water.");
+            Console.WriteLine(" Pond water temperature plays a vital role in the overal health of the eco-system and the health of pond fish. The perfect pond water should be in the range of 68 to 74 degrees Fahrenheit.");
+            Console.WriteLine(" During summer, water loses much of its ability to hold oxygen when the temperature is above 85 degrees. In winter, when the temperature goes below 39 degrees Fahrenheit fish do not have to eat.");
             Console.WriteLine("");
-            Console.WriteLine("This experiment will collect temperature data, store it in a database for later retreaval, and let you know if you should contine or stop feeding your fish based on last 7-day water temperature average.");
-
-            Console.Write("Please enter the date of the experiment...");
+            Console.WriteLine(" This experiment will collect temperature data, store it in a database for later retreaval, and let you know if you should contine or stop feeding your fish based on last 7-day water temperature average.");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine(" Please enter the date of the experiment (date/day/year format):");
+            
             string TExpDate = Console.ReadLine();
-            File.WriteAllText(@"D:/My Documents/Code Louisville/september2021/WaterQualityWithVernier/WaterQualityWithVernier/Saved Data/TExpData.txt", TExpDate);
+         
+            Console.Clear();
 
-            Console.ReadLine();
+            Console.WriteLine(" Please enter temperature recorded (in degrees Fahrenheit):");
 
+            string TExpTemp = Console.ReadLine();
 
+            List<string> DateAndTemp = new List<string>();
 
+            DateAndTemp.Add($"{TExpDate},{TExpTemp}");
 
+            File.AppendAllLines("D:/My Documents/Code Louisville/september2021/WaterQualityWithVernier/WaterQualityWithVernier/Saved Data/TExpData.csv", DateAndTemp);
 
-
-
-
-
-
-
-
-
-
+            MainMenu();
         }
 
+  
         // pH Experiment Section
         static void pHExp()
         {
