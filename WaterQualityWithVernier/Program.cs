06d0f02b17b7e3041171f1c46c264712f488fd19
Ermin Vila <83635556+ermin-code@users.noreply.github.com>
2021-10-25 03:03:13 -0400

    I created a new method for searching and displaying CSV data for temperature experiment. I also did minor fixes on menus.

--- WaterQualityWithVernier/Program.cs
+++ WaterQualityWithVernier/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using CsvHelper;
+using CsvReader;
+
+
 
 namespace WaterQualityWithVernier
 {
@@ -40,7 +44,7 @@ namespace WaterQualityWithVernier
             switch (myoptions)
             {
                 case "1":
-                    TempExp();
+                    TempExpMenu();
                     break;
 
                 case "2":
@@ -68,10 +72,39 @@ namespace WaterQualityWithVernier
         }
 
         //Temperature Experiment Section
-        public static void TempExp()
+
+        public static void TempExpMenu()
         {
             Console.Clear();
             Console.WriteLine("*********************** Temperature Experiment ***********************");
+            Console.WriteLine("");
+            Console.WriteLine("     Option 1. Enter Temperature Data");
+            Console.WriteLine("     Option 2. Search Temperature Data");
+            Console.WriteLine("     Option 3. Exit to Main Menu"); 
+
+            string myoptions;
+            myoptions = Console.ReadLine();
+            switch (myoptions)
+            {
+                case "1":
+                    TempExpEntry();
+                    break;
+
+                case "2":
+                    TempExpDataSearch();
+                    break;
+
+
+                case "3":
+                    MainMenu();
+                    break;
+            }
+        }
+
+        public static void TempExpEntry()
+        {
+            Console.Clear();
+            Console.WriteLine("*********************** Temperature Experiment Data Entry ***********************");
             Console.WriteLine("");
             Console.WriteLine(" In this experiment you will collect temperature data on your pond water using Vernier temperature probe, Texas Instruments Graphing Calculator with attached CBL2 Calculator-Based Laboratory.");
             Console.WriteLine(" Outdoor water temperatures can range from 32 degrees Celsius to above 86 degrees Celsius in the summer. In general, cooler water in pond is generally considered healthier than warmer water.");
@@ -82,14 +115,18 @@ namespace WaterQualityWithVernier
             Console.WriteLine("");
             Console.WriteLine("");
             Console.WriteLine(" Please enter the date of the experiment (date/day/year format):");
+            Console.WriteLine("");
             
             string TExpDate = Console.ReadLine();
-         
+
             Console.Clear();
 
             Console.WriteLine(" Please enter temperature recorded (in degrees Fahrenheit):");
+            Console.WriteLine("");
 
             string TExpTemp = Console.ReadLine();
+
+            Console.Clear();
 
             List<string> DateAndTemp = new List<string>();
 
@@ -97,10 +134,64 @@ namespace WaterQualityWithVernier
 
             File.AppendAllLines("D:/My Documents/Code Louisville/september2021/WaterQualityWithVernier/WaterQualityWithVernier/Saved Data/TExpData.csv", DateAndTemp);
 
-            MainMenu();
+            Console.Clear();
+            TempExpMenu();
         }
 
-  
+        //Temperature Experiment Data Search Section
+
+        public static void TempExpDataSearch()
+        {
+
+            Console.Clear();
+            Console.WriteLine("*********************** Temperature Experiment Data Search ***********************");
+            Console.WriteLine("");
+            Console.WriteLine("What date would you like to search temperature data for? (date/day/year format");
+            Console.WriteLine("");
+
+            string TExpCSV = "D:/My Documents/Code Louisville/september2021/WaterQualityWithVernier/WaterQualityWithVernier/Saved Data/TExpData.csv";
+            string TExpDate = Console.ReadLine();
+            Console.Clear();
+            char csvSeparator = ',';
+
+
+            
+
+            foreach (string line in File.ReadLines(TExpCSV))
+            {
+                foreach (string value in line.Replace("\"", "").Split('\r', '\n', csvSeparator))
+                {
+                    if (value.Trim() == TExpDate.Trim()) // case sensitive
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Temperature on " + value + " was " + line.Split(',')[1] + " degrees Fahrenheit.");
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        Console.WriteLine("Press ENTER to go back.");
+                        Console.ReadLine();
+                        Console.Clear();
+                        TempExpMenu();
+                    }
+
+                    while (value.Trim() != TExpDate.Trim())
+                    {
+                        break;
+                    }
+
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("There is no temperature data for that date!");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Press ENTER to go back");
+            Console.ReadLine();
+            Console.Clear();
+            TempExpMenu();
+            
+        }
+
         // pH Experiment Section
         static void pHExp()
         {
